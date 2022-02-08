594e4e34e86ce7605c67c467cf5ae12e449864cb
Wiktor Flis <wikflis@gmail.com>
2022-02-08 17:15:14 +0100

    Add other lights in fragment shader

--- learnopengl_tutorial/Shaders/color_cube.fs
+++ learnopengl_tutorial/Shaders/color_cube.fs
@@ -75,10 +75,10 @@ void main() {
     vec3 viewDir = normalize(viewPos - FragPos);
 
     // phase 1: Directional lighting
-    vec3 result = vec3(0.0, 0.0, 0.0); // CalcDirLight(dirLight, norm, viewDir);
+    vec3 result = vec3(0.0, 0.0, 0.0); CalcDirLight(dirLight, norm, viewDir);
     // phase 2: Point lights
-    //for(int i = 0; i < NR_POINT_LIGHTS; i++)
-    //    result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);    
+    for(int i = 0; i < NR_POINT_LIGHTS; i++)
+        result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);    
     // phase 3: Spot light
     result += CalcSpotLight(spotLight, norm, FragPos, viewDir);    
     
